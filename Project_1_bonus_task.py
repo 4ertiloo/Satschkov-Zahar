83332c312282915b3e60f164b95ed05950f7ef40
4ertiloo <z-sachkov@mail.ru>
2025-11-11 11:38:20 +0300

    Create Project_1_bonus_task.py

---
+++ Project_1_bonus_task.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+year_founded = int(input('Введи год основания вуза: '))
+current_year = datetime.now().year  # Получаем текущий год из модуля datetime
+
+age = current_year - year_founded
+print(f'Привет! Ты учишься в ВУЗе, которому сейчас {age} лет.')
+
+# Вычисление следующего юбилея
+next_anniversary = (age // 10 + 1) * 10
+next_year = year_founded + next_anniversary
+
+print(f'Следующий юбилей твоего ВУЗа (ему исполнится {next_anniversary} лет) будет в {next_year} году.')
