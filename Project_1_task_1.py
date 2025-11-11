e6b8da82d18686c556df2dedcfcf4c9f590bdab2
4ertiloo <z-sachkov@mail.ru>
2025-11-11 11:29:55 +0300

    Create Project_1_task_1.py

---
+++ Project_1_task_1.py
@@ -0,0 +1,9 @@
+name = input('Введи cвое имя: ')
+country = input('Введи свою страну: ')
+city = input('Введи свой город: ')
+university = input('Введи свой вуз: ')
+faculty = input('Введи свой факультет: ')
+
+print(f'Привет, {name}! Очень приятно познакомиться.')
+print(f'Ты из города {city}, страна {country}.')
+print(f'Рады видеть студента {university}, обучающегося на факультете {faculty}!')
